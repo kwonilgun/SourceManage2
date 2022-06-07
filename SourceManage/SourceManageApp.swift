c7f0af8aa1347a51bf1c4edd91d08372d8496a6c
kwonilgun <kwonilgun@naver.com>
2022-06-07 13:57:15 +0900

    Initial Commit

---
+++ SourceManage/SourceManageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceManageApp.swift
+//  SourceManage
+//
+//  Created by kwonilgun on 2022/06/07.
+//
+
+import SwiftUI
+
+@main
+struct SourceManageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
